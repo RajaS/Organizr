81485f2ae5c2776f221f9202b07d0e169b78de84
Raja Selvaraj <rajajs@gmail.com>
2009-10-07 17:48:45 +0530

    cleanup to improve pylint score

--- organizr.py
+++ organizr.py
@@ -23,10 +23,12 @@ import Exifreader
 #  - Allow custom sort of files
 ##########################
 
-
-ID_OPEN = wx.NewId(); ID_SAVE = wx.NewId()
-ID_EXIT = wx.NewId(); ID_PREV = wx.NewId()
-ID_NEXT = wx.NewId(); ID_ZOOMIN = wx.NewId()
+ID_OPEN = wx.NewId()
+ID_SAVE = wx.NewId()
+ID_EXIT = wx.NewId()
+ID_PREV = wx.NewId()
+ID_NEXT = wx.NewId()
+ID_ZOOMIN = wx.NewId()
 ID_ZOOMOUT = wx.NewId()
 
 # utility functions
@@ -37,18 +39,18 @@ def reduce_fraction(fraction_string):
     input '5' gives 5"""
     try:
         num, den = fraction_string.split('/')
-        reduced_string = '%0.1f' %(float(num) / float(den))
-    except:
-        reduced_string = '%s' %(fraction_string)
+        reduced_string = '%0.1f' % (float(num) / float(den))
+    except ValueError:
+        reduced_string = '%s' % (fraction_string)
     return reduced_string
 
 def get_thumbnailfile(filename):
     """for any image filename, find the stored thumbnail.
     As per free desktop specifications, this is stored in
     the .thumbnails dir in the home directory"""
-    hash = md5.new('file://'+filename).hexdigest()
+    file_hash = md5.new('file://'+filename).hexdigest()
     tb_filename = os.path.join(os.path.expanduser('~/.thumbnails/normal'),
-                                       hash) + '.png'
+                               file_hash) + '.png'
     if os.path.exists(tb_filename):
         return tb_filename
     else:
@@ -113,9 +115,10 @@ class MainFrame(wx.Frame):
         self.CURRENT_DIR = os.path.expanduser('~')
         self.WRAPON = True # wrap around in playlist
         self.AUTOROTATE = True # automatically rotate images
-        # need to have these ready before imagecanvas and thumbnail canvas are initialized
+        # need to have these ready before imagecanvas and
+        # thumbnail canvas are initialized
         self.im = Im(self) 
-        self.preview = Series_Preview(self, [])
+        self.preview = SeriesPreview(self, [])
         self.tb_file = None # filename for thumbnail
         
     def __do_layout(self):
@@ -125,7 +128,8 @@ class MainFrame(wx.Frame):
 
         sizer_2 = wx.BoxSizer(wx.HORIZONTAL)
         sizer_3 = wx.BoxSizer(wx.HORIZONTAL)
-        self.horizontal_splitter.SplitHorizontally(self.exifpanel, self.thumbnailpanel, 453)
+        self.horizontal_splitter.SplitHorizontally(self.exifpanel,
+                                                   self.thumbnailpanel, 453)
         sizer_3.Add(self.horizontal_splitter, 1, wx.EXPAND, 0)
         self.sidepanel.SetSizer(sizer_3)
         self.vertical_splitter.SplitVertically(self.canvas, self.sidepanel, 700)
@@ -176,15 +180,15 @@ class MainFrame(wx.Frame):
         
     def onopen(self, event):
         """Open a new file"""
-        filter = ''.join(['Image files|',
+        wildcard = ''.join(['Image files|',
                           '*.png;*.PNG;',
                           '*.jpg;*.jpeg;*.JPG;*.JPEG',
                           '*.tif;*.tiff;*.TIF;*.TIFF',
                           '*.bmp;*.BMP',
                           '|All files|*.*'])
 
-        dlg = wx.FileDialog(self,defaultDir = self.CURRENT_DIR,
-                            style=wx.OPEN, wildcard=filter)
+        dlg = wx.FileDialog(self, defaultDir = self.CURRENT_DIR,
+                            style=wx.OPEN, wildcard=wildcard)
         if dlg.ShowModal() == wx.ID_OK:
             self.filepath = dlg.GetPath()
             self.CURRENT_DIR = os.path.dirname(self.filepath)
@@ -233,9 +237,9 @@ class MainFrame(wx.Frame):
             self.exifpanel.SetStringItem(index, 1, info[1])
 
         # display thumbnails preview
-        self.preview  = Series_Preview(self, self.playlist[
+        self.preview  = SeriesPreview(self, self.playlist[
                         self.nowshowing-3:self.nowshowing+4])
-        self.frame.playlistcanvas.NEEDREDRAW = True
+        self.playlistcanvas.NEEDREDRAW = True
 
         # load and display image and thumbnail
         self.tb_file = get_thumbnailfile(self.playlist[self.nowshowing])
@@ -243,7 +247,6 @@ class MainFrame(wx.Frame):
         
     def on_key_down(self, event):
         """process key presses"""
-        print event.GetKeyCode()
         keycode = event.GetKeyCode()
         if keycode == 79: #'o'
             self.onopen(event)
@@ -260,7 +263,7 @@ class MainFrame(wx.Frame):
         elif keycode in [314, 315, 316, 317]: #arrow keys
             self.im.shift_zoom_frame(event)
         else:
-            pass        
+            print 'key pressed - ', keycode
         
     def create_playlist(self):
         """
@@ -268,21 +271,15 @@ class MainFrame(wx.Frame):
         from the selected file
         """
         self.playlist = []
-        dirname,currentimage = os.path.split(self.filepath)
+        dirname = os.path.dirname(self.filepath)
         allfiles = os.listdir(dirname)
                 
         for eachfile in allfiles:
-            if os.path.splitext(eachfile)[1].lower() in ['.bmp','.png',
-                                        '.jpg','.jpeg','.tif','.tiff']:
-                self.playlist.append(os.path.join(dirname,eachfile))
+            if os.path.splitext(eachfile)[1].lower() in ['.bmp', '.png',
+                                        '.jpg', '.jpeg', '.tif', '.tiff']:
+                self.playlist.append(os.path.join(dirname, eachfile))
         self.playlist.sort()
         self.nowshowing = self.playlist.index(self.filepath)
-
-        # list all thumbnail names
-        for filename in self.playlist:
-            hash = md5.new('file://'+filename).hexdigest()
-            tb_filename = os.path.join(os.path.expanduser('~/.thumbnails/normal'),
-                                       hash, '.png')
 
         
 class DisplayCanvas(wx.Panel):
@@ -291,26 +288,26 @@ class DisplayCanvas(wx.Panel):
         wx.Panel.__init__(self, parent, -1, **kwargs)
 
         self.NEEDREDRAW = False
-        self.Bind(wx.EVT_SIZE, self.OnResize)
-        self.Bind(wx.EVT_IDLE, self.OnIdle)
-        self.Bind(wx.EVT_PAINT, self.OnPaint) 
+        self.Bind(wx.EVT_SIZE, self.on_resize)
+        self.Bind(wx.EVT_IDLE, self.on_idle)
+        self.Bind(wx.EVT_PAINT, self.on_paint) 
 
-    def OnIdle(self, event):
+    def on_idle(self, event):
         """Redraw if there is a change"""
         if self.NEEDREDRAW:
             dc = wx.BufferedDC(wx.ClientDC(self), self.buffer,
                                wx.BUFFER_CLIENT_AREA)
             dc.Clear()  #clear old image if still there        
-            self.Draw(dc)
+            self.draw(dc)
             self.NEEDREDRAW = False
 
-    def ImageToBitmap(self, img):
+    def image_to_bitmap(self, img):
         newimage = apply(wx.EmptyImage, img.size)
         newimage.SetData(img.convert( "RGB").tostring())
         bmp = newimage.ConvertToBitmap()
         return bmp
 
-    def OnResize(self, event):
+    def on_resize(self, event):
         """when canvas  is resized, we create a new buffer, which will
         be redrawn on next idle event"""
         # update / initialize height and width
@@ -318,13 +315,13 @@ class DisplayCanvas(wx.Panel):
         
         # update / create the buffer for the buffered dc
         image = wx.EmptyImage(self.width, self.height)
-        wx.Image.Create(image,self.width, self.height, False)
+        wx.Image.Create(image, self.width, self.height, False)
         wx.Image.SetRGBRect(image, wx.Rect(0, 0, self.width, self.height),
                             255, 255, 255)        
         self.buffer = wx.BitmapFromImage(image)
         self.NEEDREDRAW = True
 
-    def OnPaint(self, event):
+    def on_paint(self, event):
         dc = wx.BufferedPaintDC(self, self.buffer)
 
     def get_resize_params(self, imagewidth, imageheight):
@@ -341,7 +338,7 @@ class DisplayCanvas(wx.Panel):
         self.xoffset = (self.width-self.resized_width)/2
         self.yoffset = (self.height-self.resized_height)/2
 
-    def Draw(self, dc):
+    def draw(self, dc):
         """Drawing routine.Implement in subclass"""
         pass
         
@@ -363,22 +360,22 @@ class ImageCanvas(DisplayCanvas):
     def resize_image(self):
         """Process the image by resizing to best fit current size"""
         self.resizedimage = self.frame.im.image.copy()
-        self.resizedimage.thumbnail((self.width, self.height), Image.NEAREST) #ANTIALIAS)
+        self.resizedimage.thumbnail((self.width, self.height), Image.NEAREST)
         self.resized_width, self.resized_height = self.resizedimage.size
         self.xoffset = (self.width-self.resized_width)/2
         self.yoffset = (self.height-self.resized_height)/2
         
         # blit the image centerd in x and y axes
-        self.bmp = self.ImageToBitmap(self.resizedimage)
+        self.bmp = self.image_to_bitmap(self.resizedimage)
 
         self.imagedc = wx.MemoryDC()
         self.imagedc.SelectObject(self.bmp)
 
-    def Draw(self, dc):
+    def draw(self, dc):
         """Redraw the image"""
         self.resize_image()
         # blit the buffer on to the screen
-        w, h = self.frame.im.image.size
+        #w, h = self.frame.im.image.size
         dc.Blit(self.xoffset, self.yoffset,
                 self.resized_width, self.resized_height, self.imagedc,
                 0, 0)
@@ -402,16 +399,16 @@ class PlayListCanvas(DisplayCanvas):
                                           self.resized_height)
                                              , Image.ANTIALIAS)
         # blit the image centerd in x and y axes
-        self.bmp = self.ImageToBitmap(self.resizedimage)
+        self.bmp = self.image_to_bitmap(self.resizedimage)
 
         self.imagedc = wx.MemoryDC()
         self.imagedc.SelectObject(self.bmp)
 
-    def Draw(self, dc):
+    def draw(self, dc):
         """Redraw the image"""
         self.resize_image()
         # blit the buffer on to the screen
-        w, h = self.frame.preview.composite.size
+        #w, h = self.frame.preview.composite.size
         dc.Blit(self.xoffset, self.yoffset,
                 self.resized_width, self.resized_height, self.imagedc,
                 0, 0)
@@ -443,12 +440,12 @@ class ThumbnailCanvas(DisplayCanvas):
                                           self.resized_height)
                                              , Image.NEAREST)
         # blit the image centerd in x and y axes
-        self.bmp = self.ImageToBitmap(self.resizedimage)
+        self.bmp = self.image_to_bitmap(self.resizedimage)
 
         self.imagedc = wx.MemoryDC()
         self.imagedc.SelectObject(self.bmp)
 
-    def Draw(self, dc):
+    def draw(self, dc):
         """Redraw the image"""
         self.resize_image()
         # blit the buffer on to the screen
@@ -460,15 +457,15 @@ class ThumbnailCanvas(DisplayCanvas):
         x1, y1, x2, y2 = self.frame.im.zoomframe
 
         dc.SetPen(self.pen)
-        dc.DrawLine(x1, y1, x2, y1)
-        dc.DrawLine(x2, y1, x2, y2)
-        dc.DrawLine(x2, y2, x1, y2)
-        dc.DrawLine(x1, y2, x1, y1)
+        dc.drawLine(x1, y1, x2, y1)
+        dc.drawLine(x2, y1, x2, y2)
+        dc.drawLine(x2, y2, x1, y2)
+        dc.drawLine(x1, y2, x1, y1)
 
         self.NEEDREDRAW = False 
 
 
-class Series_Preview():
+class SeriesPreview():
     """A composite image made of thumbnails from all playlist images.
     Should be triggered by opening a new file"""
     def __init__(self, parent, imagelist):
@@ -482,20 +479,25 @@ class Series_Preview():
             self.tn_size = 100 # thumbnail size
             self.blankimage = Image.new('RGB', (self.tn_size, self.tn_size),
                                         (200, 200, 200))
-            self.composite = Image.new('RGB', ((self.tn_size + 10) * len(self.filenames),
-                                               self.tn_size + 10), (255, 255, 255))
+            self.composite = Image.new('RGB', ((self.tn_size + 10) *
+                                               len(self.filenames),
+                                      self.tn_size + 10), (255, 255, 255))
             self.build_composite()
 
     def build_composite(self):
         """Build a composite image with thumbnails of the images."""
         self.im_list = []
         for filename in self.filenames:
-            # get thumbnail from nautilus store
+            # get thumbnail from nautilus store if possible
             tb_file = get_thumbnailfile(filename)
             if tb_file:
                 self.im_list.append(Image.open(tb_file))
             else:
-                self.im_list.append(self.blankimage)
+                try:
+                    self.im_list.append(Image.open(self.filename))
+                    print 'opened image instead of thumbnail'
+                except:
+                    self.im_list.append(self.blankimage)
 
         # resize
         self.thumbnails = self.im_list
@@ -504,15 +506,16 @@ class Series_Preview():
 
         # paste the thumbnails
         for index in range(len(self.im_list)):
-            w,h = self.thumbnails[index].size
+            w, h = self.thumbnails[index].size
             x1 = 5 + index * (self.tn_size + 10)
             xoffset = (self.tn_size - w) / 2
             yoffset = (self.tn_size - h) / 2
-            self.composite.paste(self.thumbnails[index], (x1 + xoffset, 5 + yoffset)) 
+            self.composite.paste(self.thumbnails[index],
+                                 (x1 + xoffset, 5 + yoffset)) 
 
         # draw box highlighting current image
         center = int(len(self.im_list) / 2)
-        draw = ImageDraw.Draw(self.composite)
+        draw = ImageDraw.draw(self.composite)
         x1 = center * (self.tn_size + 10)
         x2 = x1 + 105
         draw.line((x1, 0, x2, 0, x2, 100, x1, 100, x1, 0),
@@ -524,12 +527,12 @@ class Im():
         """imagefilenames is a list of the filenames.
         for single image this is a singleton list.
         Multiple items indicate this is a series to be loaded"""
-        self.image = Image.new('RGB', (100,200), (255,255,255))
-        self.original_image = Image.new('RGB', (100,200), (255,255,255))
+        self.image = Image.new('RGB', (100, 200), (255, 255, 255))
+        self.original_image = Image.new('RGB', (100, 200), (255, 255, 255))
 
         self.frame = parent
 
-        self.zoomframe = (0,0,0,0)
+        self.zoomframe = (0, 0, 0, 0)
         self.ZOOMSTEP = 1.1
         self.SHIFTZOOMSTEP = 5
         
@@ -545,7 +548,8 @@ class Im():
             self.frame.SetStatusText('Could not load image')
             return
 
-        self.frame.SetStatusText('Loaded in %s seconds' %(time.time() - stime), 1)
+        self.frame.SetStatusText('Loaded in %s seconds' %
+                                 (time.time() - stime), 1)
         # depending on orientation info in exif, rotate the image
         if self.frame.AUTOROTATE:
             try:
@@ -554,7 +558,8 @@ class Im():
                 pass # no exif orientation info    
 
         self.width, self.height = self.original_image.size
-        self.zoom_xoffset = None; self.zoom_yoffset = None
+        self.zoom_xoffset = None
+        self.zoom_yoffset = None
         self.zoom_ratio = 1
         self.image = self.original_image
         
@@ -575,7 +580,7 @@ class Im():
         If offsets are not given center the zoom
         """
         scale = self.zoom_ratio
-        xoffset = self.zoom_xoffset; yoffset = self.zoom_yoffset
+        #xoffset = self.zoom_xoffset; yoffset = self.zoom_yoffset
         frame = []
         newwidth = self.width / scale
         newheight = self.height / scale
@@ -583,15 +588,19 @@ class Im():
         if not self.zoom_xoffset:
             self.zoom_xoffset = (self.width - newwidth) / 2
         else:
-            self.zoom_xoffset = min(self.zoom_xoffset, (self.width - newwidth) / 2)
+            self.zoom_xoffset = min(self.zoom_xoffset,
+                                    (self.width - newwidth) / 2)
 
         if not self.zoom_yoffset:
             self.zoom_yoffset = (self.height - newheight) / 2
         else:
-            self.zoom_yoffset = min(self.zoom_yoffset, (self.height - newheight) / 2)
+            self.zoom_yoffset = min(self.zoom_yoffset,
+                                    (self.height - newheight) / 2)
 
-        self.zoomframe = [int(value) for value in [self.zoom_xoffset, self.zoom_yoffset,
-                     self.zoom_xoffset + newwidth, self.zoom_yoffset + newheight]]
+        self.zoomframe = [int(value) for value in
+                          [self.zoom_xoffset, self.zoom_yoffset,
+                           self.zoom_xoffset + newwidth,
+                           self.zoom_yoffset + newheight]]
         #
         if self.zoom_xoffset < 0:
             self.zoom_xoffset = 0
@@ -716,26 +725,6 @@ def main():
     """
     organizr = Organizr()
     organizr.MainLoop()
-
-
-def resizetest():
-    import glob
-    filter = '/data/pics/Aug2009/*.jpg'
-    jpgfiles = glob.glob(filter)
-    num = len(jpgfiles)
-
-    for file in jpgfiles:
-        stime = time.time()
-        im = Image.open(file)
-        dummy = im.resize((100, 100))
-    print 'total time ', time.time() - stime
-    
-    for file in jpgfiles:
-        stime = time.time()
-        print 'starting thumbnail'
-        im = Image.open(file)
-        im.thumbnail((100, 100), Image.ANTIALIAS)
-    print 'total time ', time.time() - stime
     
 
 if __name__ == '__main__':
